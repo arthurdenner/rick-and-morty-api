647d3f3ddf6a2213c66094dbbc4c61d3682b6bb3
arthurdenner <arthurdenner7@gmail.com>
2018-03-18 11:33:02 -0300

    Creating a simple wrapper around the REST endpoint

---
+++ graphql/resolvers.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+const baseURL = 'https://rickandmortyapi.com/api';
+
+const api = axios.create({
+  baseURL,
+});
+
+const resolvers = {
+  Query: {
+    character(_, { id }) {
+      return api.get(`character/${id}`).then(({ data }) => data);
+    },
+    characters(_, { page = 1 }) {
+      // TODO: add pagination info?
+      return api
+        .get(`character/?page=${page}`)
+        .then(({ data }) => data.results);
+    },
+    episode(_, { id }) {
+      return api.get(`episode/${id}`).then(({ data }) => data);
+    },
+    episodes(_, { page = 1 }) {
+      // TODO: add pagination info?
+      return api.get(`episode/?page=${page}`).then(({ data }) => data.results);
+    },
+    location(_, { id }) {
+      return api.get(`location/${id}`).then(({ data }) => data);
+    },
+    locations(_, { page = 1 }) {
+      // missing pagination info
+      return api.get(`location/?page=${page}`).then(({ data }) => data.results);
+    },
+  },
+  Character: {
+    episodes(character) {
+      const urls = character.episode.map(url =>
+        axios.get(url).then(({ data }) => data)
+      );
+
+      return axios.all(urls).then(axios.spread((...episodes) => episodes));
+    },
+    location({ location }) {
+      if (!location.url) {
+        return location;
+      }
+
+      return axios.get(location.url).then(({ data }) => data);
+    },
+    origin({ origin }) {
+      if (!origin.url) {
+        return origin;
+      }
+
+      return axios.get(origin.url).then(({ data }) => data);
+    },
+  },
+};
+
+export default resolvers;
--- server.js
+++ server.js
@@ -1,53 +1,21 @@
-require('dotenv').config();
+import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import schema from './graphql/schema';
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-const morgan = require('morgan')
+const GRAPHQL_PORT = 3000;
 
-const app = express();
+const graphQLServer = express();
 
-const api = require('./routes/api')
-const errors = require('./handlers/errors')
+graphQLServer.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({ schema, cacheControl: true })
+);
+graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-const db = process.env.NODE_ENV === "production" ? process.env.DATABASE : 'mongodb://localhost:27017/rickmorty-api'
-
-mongoose.connect(db)
-mongoose.Promise = global.Promise
-
-mongoose.connection.on('error', err => {
-  console.error(`→ ${err.message}`);
-});
-
-if (app.get('env') !== 'test') {
-  app.use(morgan('dev', {
-    skip(req) {
-      return req.path.match(/(ico|png|svg|jpeg|woff2|css|js|txt|)$/ig)[0] ? true : false
-    }
-  }))
-}
-
-app.set('trust proxy', 1);
-
-app.use(express.static(path.join(__dirname, 'static')));
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => res.redirect('/api'))
-app.use('/api', api)
-
-app.use(errors.notFound);
-app.use(errors.productionErrors);
-
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log('\x1b[34m%s\x1b[0m', `
-  ${app.get('env').toUpperCase()}
-
-  Port      → http://localhost:${PORT}
-  Database  → ${mongoose.connection.host}/${mongoose.connection.name}
-  `
-))
-
-module.exports = app
+graphQLServer.listen(GRAPHQL_PORT, () =>
+  console.log(
+    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
+  )
+);
